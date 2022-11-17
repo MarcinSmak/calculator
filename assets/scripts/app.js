abce29568b7060a63b9742333f49235631ea207c
Fulcrum <fulcrum81@tutanota.com>
2022-11-17 23:11:12 +0100

    executing funcion add undirectly

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,13 +1,9 @@
 const defaultResult = 0;
 let currentResult = defaultResult;
 
-function add(num1, num2) {
-    const result = num1 + num2;
-    return result;
+function add() {
+    currentResult = currentResult + userInput.value;
+    outputResult(currentResult, '');
 }
 
-currentResult = add(1, 2);
-
-let calculationDescription = 'Decription';
-
-outputResult(currentResult, calculationDescription);
+addBtn.addEventListener('click', add);
