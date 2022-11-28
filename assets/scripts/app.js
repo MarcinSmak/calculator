ddb5b28a9ec5f8a757ba630bd83cc33dbb3390b5
Fulcrum <fulcrum81@tutanota.com>
2022-11-28 21:03:02 +0100

    calculator decription added

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -2,8 +2,9 @@ const defaultResult = 0;
 let currentResult = defaultResult;
 
 function add() {
+    calcDescription = `${currentResult} + ${userInput.value}`;
     currentResult = currentResult + parseInt(userInput.value);
-    outputResult(currentResult, '');
+    outputResult(currentResult, calcDescription);
 }
 
 addBtn.addEventListener('click', add);
